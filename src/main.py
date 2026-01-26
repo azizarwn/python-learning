148557c9fd963be63b785bddf527d271115020bd
azizarwn <azzahrawani98@gmail.com>
2026-01-27 00:26:09 +0700

    add : install python and uv init

---
+++ src/main.py
@@ -0,0 +1,18 @@
+print("Hello")
+print("Hello " * 3)
+print("Hello" + " World")
+
+name = "Lulu"
+name = 3
+
+print(name)
+
+
+# check version python
+# python --version
+
+# ---install UV---
+# powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+# add PATH uv
+# uv --version 
+# uv init
