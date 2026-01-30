5948289adee3b8b8ea3c3f7494fd7a75bfe10f85
azizarwn <azzahrawani98@gmail.com>
2026-01-30 23:23:23 +0700

    feat : looping case

---
+++ src/day_2/looping.py
@@ -0,0 +1,20 @@
+# case 1
+cat_food = ""
+print("--- Case 1 ---")
+if cat_food == "lamb":
+    print("Oyen don't like it")
+elif cat_food == "fish":
+    print("Oyen like it")
+else:
+    print("Find cat food!")
+print("")
+
+# case 2
+year = 2022
+print("--- Case 2 ---")
+if year >= 2024:
+    print("it's new normal year :D")
+elif year >= 2020:
+    print("it's covid year :'(")
+else:
+    print("life so far so good :)")
